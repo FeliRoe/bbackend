06b1689125b084db7fb047f1f40e57d18d41e8d1
wuestm <94378838+wuestm@users.noreply.github.com>
2023-03-14 13:23:23 +0100

    frontend mit bisschen fetchen

--- app/routes/interessenten.js
+++ app/routes/interessenten.js
@@ -1,9 +1,9 @@
 const execute = require('../execute/interessenten');
 const router = require('express').Router();
 
-router
-  .get('/', execute.holalles) 
-  .get('/:inseratId', execute.getNumber) 
-  .post('/', execute.intressezeigen)  
+
+  router.get('/', execute.holalles);
+  router.get('/:inseratId', execute.getNumber);
+  router.post('/', execute.intressezeigen);
 
 module.exports = router;
